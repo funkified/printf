96f5d969c77b76ebad941e378c82211ae2228db1
Sergio Vera <sergio.vera@icloud.com>
2021-07-26 21:52:54 +0000

    betty

--- _printf.c
+++ _printf.c
@@ -1,16 +1,13 @@
 #include "holberton.h"
-
 /**
  */
-
 int _printf(const char *format, ...)
 {
 	va_list arg;
 	int i = 0;
 
 	va_start(arg, format);
-
-	if (format == NULL)
+	if (arg == NULL && format == NULL)
 		return (-1);
 	while (*format != '\0')
 	{
@@ -18,52 +15,47 @@ int _printf(const char *format, ...)
 		{
 			switch (*(format + 1))
 			{
-				case '\0' :
-						return (-1);
-				case '%' :
-						i += _putchar('%');
-						format++;
-						break;
-				case 'c' : 
-						i += print_char(arg);
-						format++;
-						break;
-				case 'd' :
-						i = va_arg(arg, long int);
-						if (i < 0)
-						{
-							i = -i;
-							_putchar('-');
-						}
-						_puts(convert(i, 10));
-						format++;
-						break;
-					case 'i' :
-						i = va_arg(arg, long int);
-						_puts(convert(i, 10));
-						format++;
-						break;
-				case 'o' : 
-						i = va_arg(arg, long int);
-					    _puts(convert(i, 8));
-						format++;
-						break;
-				case 's' :
-						   i += print_string(arg);
-						   format++;
-						   break;
-				case 'x' :
-						   i = va_arg(arg, unsigned int);
-						   _puts(convert(i, 16));
-						   format++;
-						   break;
+				case '\0':
+					return (-1);
+				case '%':
+					i += _putchar('%');
+					format++;
+					break;
+				case 'c':
+					i += print_char(arg);
+					format++;
+					break;
+				case 'd':
+					i = va_arg(arg, long int);
+					if (i < 0)
+					{
+						i = -i;
+						_putchar('-');
+					}
+					_puts(convert(i, 10));
+					format++;
+					break;
+				case 'i':
+					i = va_arg(arg, long int);
+					_puts(convert(i, 10));
+					format++;
+					break;
+				case 'o':
+					i = va_arg(arg, long int);
+					_puts(convert(i, 8));
+					format++;
+					break;
+				case 's':
+					i += print_string(arg);
+					format++;
+					break;
 				default:
-						   i += _putchar('%');
+					i += _putchar('%');
 			}
 		}
-		else 
+		else
 			i += _putchar(*format);
-	format++;
+		format++;
 	}
 	va_end(arg);
 	return (i);
